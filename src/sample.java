ff52749f4ad31710c5804fbbed4ec17444273c10
ssinha19 <ssinha19@LIN66015292.corp.capgemini.com>
2021-10-28 11:47:56 +0530

    my first commit

---
+++ src/sample.java
@@ -0,0 +1,10 @@
+
+class sample {
+
+	public static void main(String[] args) {
+		System.out.println("it is sample file");
+		
+
+	}
+
+}
---
+++ src/sampleClass.java
@@ -0,0 +1,9 @@
+
+public class sampleClass {
+
+	public static void main(String[] args) {
+		System.out.println("it is for sample");
+
+	}
+
+}
